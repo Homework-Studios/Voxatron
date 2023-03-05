e0fd8a01dec816f117f1dca695cabf8a2756eebf
DerCooleVonDem <75242580+DerCooleVonDem@users.noreply.github.com>
2023-03-05 19:50:58 +0100

    Textures go Brrr

--- Voxatron-Engine/src/render/scene/ui/MainMenuUIScreen.java
+++ Voxatron-Engine/src/render/scene/ui/MainMenuUIScreen.java
@@ -4,6 +4,7 @@ import math.Vector2;
 import render.ui.UIScreen;
 import render.ui.box.BoxFilter;
 import render.ui.item.basic.UiRoundBoxItem;
+import render.ui.item.image.UiImageItem;
 import render.ui.item.text.UITextBoxItem;
 
 import static com.raylib.Jaylib.*;
@@ -13,6 +14,7 @@ public class MainMenuUIScreen extends UIScreen {
     public MainMenuUIScreen() {
         super(new Vector2(), new Vector2(GetScreenWidth(), GetScreenHeight()));
 
-        Vector2 full = new Vector2(GetScreenWidth() - 10, GetScreenHeight() - 10);
+        String path = System.getProperty("user.dir") + "\\Voxatron-Engine\\src\\window\\icon.png";
+        addItem(new UiImageItem(LoadTexture(path), new Vector2(0, 0), BoxFilter.CENTER));
     }
 }
---
+++ Voxatron-Engine/src/render/task/ui/UiTextureRenderTask.java
@@ -0,0 +1,24 @@
+package render.task.ui;
+
+import com.raylib.Raylib;
+import math.Vector2;
+import render.task.RenderTask;
+
+import static com.raylib.Jaylib.WHITE;
+import static com.raylib.Raylib.DrawTexture;
+
+public class UiTextureRenderTask extends RenderTask {
+
+    public Vector2 position;
+    public Raylib.Texture texture;
+
+    public UiTextureRenderTask(Vector2 position, Raylib.Texture texture) {
+        this.position = position;
+        this.texture = texture;
+    }
+
+    @Override
+    public void render() {
+        DrawTexture(texture, (int)position.x, (int)position.y, WHITE);
+    }
+}
