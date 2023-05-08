a6be53170ed6c4b42ab31492b2d5b8472c384f82
DerCooleVonDem <jonaswindmann@gmail.com>
2023-05-08 11:56:15 +0200

    Remove the most shittiest code worldwide.

--- Voxatron-Engine/src/Main.java
+++ Voxatron-Engine/src/Main.java
@@ -14,20 +14,21 @@ import java.util.Objects;
 
 
 public class Main {
+
     public static void main(String[] args) throws InterruptedException {
         if (args.length != 0) DevelopmentConstants.DEVELOPMENT_MODE = Objects.equals(args[0], "1");
         new Input();
         Point p = new Point(0, 0);
         Dimension dim = new Dimension(0, 0);
-        Asset.ASSET_DIR = DevelopmentConstants.ASSET_DIR;
+        Asset.ASSET_DIR = System.getProperty("user.dir") + "\\Voxatron\\Assets";
+        System.out.println(Asset.ASSET_DIR);
         try {
             new ScriptingManager();
         } catch (ScriptException | IOException e) {
             throw new RuntimeException(e);
         }
 
-        //Testing used to skip the engine and only test scripts if(true) return; cause of intellij errors
-//        if (true) return;
+        // if (true) return;
 
         if (DevelopmentConstants.DEVELOPMENT_MODE) {
             DevelopmentConstants.ENGINE_FORM = new EngineForm();
--- Voxatron-Engine/src/engine/DevelopmentConstants.java
+++ Voxatron-Engine/src/engine/DevelopmentConstants.java
@@ -2,9 +2,6 @@ package engine;
 
 public class DevelopmentConstants {
     public static boolean DEVELOPMENT_MODE = false;
-    public static String ASSET_DIR = "C:\\Users\\timon\\Desktop\\Coding_files\\java\\Voxatron\\Voxatron\\Assets";
-    public static String NEW_SCRIPT_DIR = "C:\\Users\\timon\\Desktop\\Coding_files\\java\\Voxatron\\Voxatron-Engine\\src\\engine\\scripts";
-    public static boolean SCRIPT_UPDATE = false;
 
     //Nicht bearbeiten
     public static EngineForm ENGINE_FORM;
